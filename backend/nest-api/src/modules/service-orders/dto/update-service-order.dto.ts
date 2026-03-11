4d3fee95586e904f291322e65e0d654d7986f4b6
Matheus Reis <matheus.reis@infortechms.com.br>
2026-03-11 16:49:49 -0400

    chore: separate backend into own repo, update .gitignore

--- backend/nest-api/src/modules/service-orders/dto/update-service-order.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateServiceOrderDto } from './create-service-order.dto';
-
-export class UpdateServiceOrderDto extends PartialType(CreateServiceOrderDto) {}
